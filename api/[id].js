a7bdd335d3bd79927b519c601a19f2c82b6cca12
Ananthan <ananthan.rajasekharan@gmail.com>
2023-12-12 14:42:23 +0530

    Update [id].js

--- api/[id].js
+++ api/[id].js
@@ -2,8 +2,8 @@ const MetaDataSet = require("../MetaDataSet");
 
 module.exports = async (req, res) => {
     const host = req.headers.host;
-    if (host === "opensea.io") {
-        const { id } = req.query;
+    const { id } = req.query;
+    if (host === "opensea.io") {        
         res.json(JSON.parse(JSON.stringify(MetaDataSet[id])))
     } else {
         return res.redirect(301, `https://opensea.io/assets/matic/0xf9ca13e465C35d8074973E62fcfDC062BF8A62Cb/${id}`);
