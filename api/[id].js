d4c81770cac46ea986ba8a8d8daf2449259ef0db
Ananthan <ananthan.rajasekharan@gmail.com>
2023-12-13 07:54:32 +0530

    :construction: | working on it

--- api/[id].js
+++ api/[id].js
@@ -3,9 +3,9 @@ const MetaDataSet = require("../MetaDataSet");
 module.exports = async (req, res) => {
     const host = req.headers.host;
     const { id } = req.query;
-    if (host === "opensea.io") {        
+    // if (host === "opensea.io") {        
         res.json(JSON.parse(JSON.stringify(MetaDataSet[id])))
-    } else {
-        return res.redirect(301, `https://opensea.io/assets/matic/0xf9ca13e465C35d8074973E62fcfDC062BF8A62Cb/${id}`);
-    }   
+    // } else {
+    //     return res.redirect(301, `https://opensea.io/assets/matic/0xf9ca13e465C35d8074973E62fcfDC062BF8A62Cb/${id}`);
+    // }   
 }
